868f095f7cdb0d504d509c0f45bf6a0af80f083b
John Taggart <taggartj1985@gmail.com>
2020-03-11 17:41:09 +0000

    exc A, adds Polmont and prints linlithgow stop

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -5,7 +5,9 @@ stops.push ("Edinburgh Waverley")
 #2. Add "Glasgow Queen St" to the start of the array
 stops.unshift ("Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, 'Polmont')
 #4. Print out the index position of "Linlithgow"
+p stops [5]
 #5. Remove "Livingston" from the array using its name
 #6. Delete "Cumbernauld" from the array by index
 #7. Print the number of stops there are in the array?
